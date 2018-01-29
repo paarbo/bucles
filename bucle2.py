f73ae74702cff155e821dc06d52e1bb1bb532f19
root <root@localhost.localdomain>
2018-01-29 12:52:28 +0100

    first commit

---
+++ bucle2.py
@@ -0,0 +1,7 @@
+num1=float(input("Introduce un numero: "))
+num2=float(input("Introduce otro mayor al anterior: "))
+num=0
+while num1>num2:
+    num2=float(input("Pon otro: "))
+print("Los numeros escritos son",num2,"y",num1)
+num+=1
