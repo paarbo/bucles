26b0707141d46d1831328fb1da08eb49f1ce91ac
root <root@localhost.localdomain>
2018-02-01 13:14:13 +0100

    first commit

---
+++ bucle6.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+fin=int(input("Establece un valor limite: "))
+num1=0
+num2=0
+x=0
+while fin>x:
+    num1=int(input("Introduce un numero: "))
+    num2=int(input("Introduce un numero: "))
+    x=num1
+print("Has superado el limite con",(x))
---
+++ nums.py
@@ -0,0 +1,5 @@
+num=int(input("Introduce un numero: "))
+i=0
+while i<num:
+    i=i+1
+    print(num)
