ef424d4051a597fa55cf6b008fcd5a232108dc48
ism23843564 <ism23843564@a13.informatica.escoladeltreball.org>
2018-02-05 13:16:07 +0100

    first commit

---
+++ bucle7.py
@@ -0,0 +1,8 @@
+num1=int(input("Introduce limite 1: "))
+num2=int(input("Introduce limite : "))
+num3=0
+x=0
+while num1<num2 and num3<num2:
+    num3=int(input("Introduce numeros: "))
+    x=x+1
+print("Has escrito",x,"numeros entre el",num1,"y",num2)
---
+++ bucle8.py
@@ -0,0 +1,14 @@
+par=0
+S=0
+N=0
+sino=0
+A=0
+while (A==0):
+	if (A==0):
+		par=int(input("Introduce un numero par: "))
+	if (par%2):
+		break
+	if (A==0):
+		sino=input("Seguir escribiendo(S/N): ")
+	if (sino=="N"):
+		break
